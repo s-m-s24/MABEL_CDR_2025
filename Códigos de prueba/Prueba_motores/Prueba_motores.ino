7a7c6795094d60b85f6ae6465e70bb279cc1758f
Sofía Macarena Sama <48045471@est.ort.edu.ar>
2025-08-04 01:53:25 -0300

    ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Códigos de prueba/Prueba_motores/Prueba_motores.ino
+++ Códigos de prueba/Prueba_motores/Prueba_motores.ino
@@ -1,33 +1,35 @@
 #include <TimerOne.h>
 
 //Motores
-int const PIN_PMW_DER = 7;
-int const PIN_PMW_IZQ = 6;
+int const PIN_PMW_DER = 5;
+//int const PIN_PMW_IZQ = 6;
 int pmwDer = 255;
-int pmwIzq = 255;
-int const PIN_MOTOR_DER_A = 5;
-int const PIN_MOTOR_DER_B = 4;
-int const PIN_MOTOR_IZQ_A = 8;
-int const PIN_MOTOR_IZQ_B = 3;
+//int pmwIzq = 255;
+int const PIN_MOTOR_DER_A = 3;
+int const PIN_MOTOR_DER_B = 2;
+//int const PIN_MOTOR_IZQ_A = 8;
+//int const PIN_MOTOR_IZQ_B = 3;
 int ms = 0;
 int seg = 0;
 
 void setup() 
 {
-
+Serial.begin(9600);
   Timer1.initialize(1000); //Cada cuantos milisegundos queremos que interrumpa el timer, en este caso: 1000
   Timer1.attachInterrupt(ISR_Timer); //A dónde queremos ir en la interrupción, en este caso: ISR_Timer (la función más abajo);
   
   digitalWrite(PIN_PMW_DER, pmwDer);
-  digitalWrite(PIN_PMW_IZQ, pmwIzq);
+ // digitalWrite(PIN_PMW_IZQ, pmwIzq);
   pinMode(PIN_MOTOR_DER_A, OUTPUT);
   pinMode(PIN_MOTOR_DER_B, OUTPUT);
-  pinMode(PIN_MOTOR_IZQ_A, OUTPUT);
-  pinMode(PIN_MOTOR_IZQ_B, OUTPUT);
+  //pinMode(PIN_MOTOR_IZQ_A, OUTPUT);
+  //pinMode(PIN_MOTOR_IZQ_B, OUTPUT);
 }
 
 void loop() 
 {
+
+  adelante();
   digitalWrite(PIN_PMW_DER, pmwDer);
   
   /*
@@ -47,8 +49,8 @@ void adelante()
   digitalWrite(PIN_MOTOR_DER_A, HIGH);
   digitalWrite(PIN_MOTOR_DER_B, LOW);
 
-  digitalWrite(PIN_MOTOR_IZQ_A, HIGH);
-  digitalWrite(PIN_MOTOR_IZQ_B, LOW);
+  //digitalWrite(PIN_MOTOR_IZQ_A, HIGH);
+  //digitalWrite(PIN_MOTOR_IZQ_B, LOW);
 }
 
 void atras()
@@ -56,8 +58,8 @@ void atras()
   digitalWrite(PIN_MOTOR_DER_A, LOW);
   digitalWrite(PIN_MOTOR_DER_B, HIGH);
 
-  digitalWrite(PIN_MOTOR_IZQ_A, LOW);
-  digitalWrite(PIN_MOTOR_IZQ_B, HIGH);
+  //digitalWrite(PIN_MOTOR_IZQ_A, LOW);
+  //digitalWrite(PIN_MOTOR_IZQ_B, HIGH);
 }
 
 void izquierda()
@@ -65,8 +67,8 @@ void izquierda()
   digitalWrite(PIN_MOTOR_DER_A, LOW);
   digitalWrite(PIN_MOTOR_DER_B, HIGH);
 
-  digitalWrite(PIN_MOTOR_IZQ_A, HIGH);
-  digitalWrite(PIN_MOTOR_IZQ_B, LOW);
+ // digitalWrite(PIN_MOTOR_IZQ_A, HIGH);
+ // digitalWrite(PIN_MOTOR_IZQ_B, LOW);
 }
 
 void derecha()
@@ -74,8 +76,8 @@ void derecha()
   digitalWrite(PIN_MOTOR_DER_A, HIGH);
   digitalWrite(PIN_MOTOR_DER_B, LOW);
 
-  digitalWrite(PIN_MOTOR_IZQ_A, LOW);
-  digitalWrite(PIN_MOTOR_IZQ_B, HIGH);
+ // digitalWrite(PIN_MOTOR_IZQ_A, LOW);
+  //digitalWrite(PIN_MOTOR_IZQ_B, HIGH);
 }
 
 void frenar()
@@ -83,8 +85,8 @@ void frenar()
   digitalWrite(PIN_MOTOR_DER_A, LOW);
   digitalWrite(PIN_MOTOR_DER_B, LOW);
 
-  digitalWrite(PIN_MOTOR_IZQ_A, LOW);
-  digitalWrite(PIN_MOTOR_IZQ_B, LOW);
+  //digitalWrite(PIN_MOTOR_IZQ_A, LOW);
+//  digitalWrite(PIN_MOTOR_IZQ_B, LOW);
 }
 
 //Función del timer
